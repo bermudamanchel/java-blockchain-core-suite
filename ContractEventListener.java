87bf6eda7485dbe4629a4dca5e8f05f6babf5d52
bermudamanchel <bermudamanchel@cdzhongguan.com>
2026-04-08 21:39:42 +0800

    Update ContractEventListener.java

--- ContractEventListener.java
+++ ContractEventListener.java
@@ -1,6 +1,6 @@
 // 智能合约事件监听
 public class ContractEventListener {
     public void listen(String event) {
-        System.out.println("监听到合约事件：" + event);
+        System.out.println("监听到合约事件:" + event);
     }
 }
